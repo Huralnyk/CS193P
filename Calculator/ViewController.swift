228d2f07242bd1bff68f9f918c6e0a4a1263ed8f
Oleksii Huralnyk <o.huralnyk@nullgr.com>
2017-02-27 23:39:43 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Oleksii Huralnyk on 2/27/17.
+//  Copyright © 2017 Oleksii Huralnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = .pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+}
+
